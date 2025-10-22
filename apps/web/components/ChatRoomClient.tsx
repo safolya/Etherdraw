9e31b33062a7085a2d6c61cad64fa7fb60e5dbcd
Safolya Mondal <safolyamondal12345@gmail.com>
2025-10-23 03:14:29 +0530

    components

--- apps/web/components/ChatRoomClient.tsx
+++ apps/web/components/ChatRoomClient.tsx
@@ -1,5 +1,27 @@
 "use client";
 
+import { useState,useEffect } from "react";
+import { useSocket } from "../hooks/useSocket";
+
 export default function ChatRoomClient({message,id}: {id: string,message:{message:string}[]}){
-    
+    const {socket, loading} = useSocket();
+    const [chats, setChats]= useState(message);
+    useEffect(()=>{
+     if(socket && !loading){
+        socket.send(JSON.stringify({
+            type:"join_room",
+            roomId:id
+        }));
+
+
+        socket.onmessage=(event)=>{
+            const data=JSON.parse(event.data);
+            if(data.type === "chat"){
+                setChats(c => [...c,data.message])
+            }
+        }
+
+     }
+
+    },[socket,loading,id])
 }
